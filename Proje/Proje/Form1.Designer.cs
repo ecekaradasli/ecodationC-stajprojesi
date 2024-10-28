dff6622839156a7d6e5c7d087f0795a6f71f9554
ecekaradasli <133108231+ecekaradasli@users.noreply.github.com>
2024-10-28 16:12:40 +0300

    Add files via upload

---
+++ Proje/Proje/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿
+namespace Proje
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comment = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.image = new System.Windows.Forms.PictureBox();
+            this.burc = new System.Windows.Forms.ComboBox();
+            this.burcresim = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.image)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comment
+            // 
+            this.comment.AutoSize = true;
+            this.comment.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comment.Location = new System.Drawing.Point(624, 251);
+            this.comment.Name = "comment";
+            this.comment.Size = new System.Drawing.Size(505, 90);
+            this.comment.TabIndex = 11;
+            this.comment.Text = resources.GetString("comment.Text");
+            this.comment.Click += new System.EventHandler(this.comment_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(344, 151);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 39);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "BURÇLAR";
+            // 
+            // image
+            // 
+            this.image.Image = global::Proje.Properties.Resources.koc;
+            this.image.Location = new System.Drawing.Point(312, 251);
+            this.image.Name = "image";
+            this.image.Size = new System.Drawing.Size(250, 200);
+            this.image.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.image.TabIndex = 9;
+            this.image.TabStop = false;
+            this.image.Click += new System.EventHandler(this.image_Click);
+            // 
+            // burc
+            // 
+            this.burc.FormattingEnabled = true;
+            this.burc.Items.AddRange(new object[] {
+            "Koç",
+            "Boğa",
+            "İkizler",
+            "Yengeç",
+            "Aslan",
+            "Başak",
+            "Terazi",
+            "Akrep",
+            "Yay",
+            "Oğlak",
+            "Kova",
+            "Balık"});
+            this.burc.Location = new System.Drawing.Point(123, 251);
+            this.burc.Name = "burc";
+            this.burc.Size = new System.Drawing.Size(125, 21);
+            this.burc.TabIndex = 8;
+            this.burc.Tag = "";
+            this.burc.Text = "Koç";
+            this.burc.SelectedIndexChanged += new System.EventHandler(this.burc_SelectedIndexChanged);
+            // 
+            // burcresim
+            // 
+            this.burcresim.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("burcresim.ImageStream")));
+            this.burcresim.TransparentColor = System.Drawing.Color.Transparent;
+            this.burcresim.Images.SetKeyName(0, "akrep.PNG");
+            this.burcresim.Images.SetKeyName(1, "aslan.PNG");
+            this.burcresim.Images.SetKeyName(2, "balik.PNG");
+            this.burcresim.Images.SetKeyName(3, "basak.PNG");
+            this.burcresim.Images.SetKeyName(4, "boga.PNG");
+            this.burcresim.Images.SetKeyName(5, "ikizler.PNG");
+            this.burcresim.Images.SetKeyName(6, "koc.PNG");
+            this.burcresim.Images.SetKeyName(7, "kova.PNG");
+            this.burcresim.Images.SetKeyName(8, "oglak.PNG");
+            this.burcresim.Images.SetKeyName(9, "terazi.PNG");
+            this.burcresim.Images.SetKeyName(10, "yay.PNG");
+            this.burcresim.Images.SetKeyName(11, "yengec.PNG");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1285, 625);
+            this.Controls.Add(this.comment);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.image);
+            this.Controls.Add(this.burc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.image)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label comment;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox image;
+        private System.Windows.Forms.ComboBox burc;
+        private System.Windows.Forms.ImageList burcresim;
+    }
+}
+
---
+++ Proje/Proje/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void image_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void burc_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (burc.Text == "Koç")
+            {
+                comment.Text = "12 burcun ilki olan Koç burcu Zodyak’ın başında yer alır. \nGüneş, her bahar bu burcu 20 Mart-19 Nisan tarihleri arasında \nziyaret eder. Yıldız haritanızda Koç burcu gezegen yerleşimine sahip \nolmasanız da pek çok gezegen yıl boyunca Koç burcundan geçerek onun \nyakıcı enerjisini harekete geçirdiği için bu burcun enerjisini anlamak önemlidir.";
+
+                image.Image = burcresim.Images["koc.png"];
+            }
+            else if (burc.Text == "Boğa")
+            {
+                comment.Text = "Zodyak’ın 12 burcundan ikinci olan Boğa burcunun döngüsü her yılın \n20 Nisan – 20 Mayıs tarihleri arasında başlar. \nYıldız haritanızda Boğa burcu gezegen yerleşimine sahip olsanız da olmasanız \nda yıl boyunca çeşitli gezegenler Boğa burcunu ziyaret ederek bu burcun \ntoprak enerjisini uyandırdığı için bu enerjiyi anlamak önemlidir.";
+
+                image.Image = burcresim.Images["boga.png"];
+            }
+            else if (burc.Text == "İkizler")
+            {
+                comment.Text = "Zodyak’ın 12 burcunun üçüncüsü olan İkizler burcunun döngüsü \nilkbaharın sonlarında başlar, 21 Mayıs – 20 Haziran tarihleri \narasına denk gelir. Yıldız haritanızda İkizler burcu gezegen yerleşimine \nsahip olmasanız da pek çok gezegen yıl boyunca İkizler burcunun rüzgarlı \nenerjisini harekete geçirerek bu burçtan geçtiği için bu enerjiyi anlamak önemlidir.";
+
+                image.Image = burcresim.Images["ikizler.png"];
+            }
+            else if (burc.Text == "Yengeç")
+            {
+                comment.Text = "Zodyak’ın 12 burcundan dördüncü burç olan Yengeç burcunun döngüsü her yılın \n21 Haziran – 22 Temmuz tarihleri arasında başlar. \nYıldız haritanızda Yengeç burcu gezegen yerleşimine sahip olmasanız da\n yıl boyunca çeşitli gezegenler Yengeç burcunu ziyaret ederek bu burcun \nsu enerjisini uyandırdığı için bu enerjiyi anlamak önemlidir.";
+
+                image.Image = burcresim.Images["yengec.png"];
+            }
+            else if (burc.Text == "Aslan")
+            {
+                comment.Text = "Zodyak’ın 12 burcundan beşinci burç olan Aslan burcunun döngüsü her yılın \n23 Temmuz – 22 Ağustos tarihleri arasında gerçekleşir. \nYıldız haritanızda Aslan burcu gezegen yerleşimine sahip olmasanız da \nyıl boyunca çeşitli gezegenler Aslan burcunu ziyaret ederek \nateş enerjisini uyandırdığı için bu enerjiyi anlamak önemlidir.";
+
+                image.Image = burcresim.Images["aslan.png"];
+            }
+            else if (burc.Text == "Başak")
+            {
+                comment.Text = "Zodyak’ın 12 burcundan altıncısı olan Başak burcunun döngüsü her yılın \n23 Ağustos – 22 Eylül tarihleri arasında gerçekleşir. \nYıldız haritanızda Başak burcu gezegen yerleşimine sahip olmasanız da yıl\n boyunca çeşitli gezegenler Başak burcunu ziyaret ederek bu burcun \ntoprak enerjisini uyandırdığı için bu enerjiyi anlamak önemlidir.";
+
+                image.Image = burcresim.Images["basak.png"];
+            }
+            else if (burc.Text == "Terazi")
+            {
+                comment.Text = "Zodyak’ın 12 burcundan yedincisi olan terazi burcunun döngüsü her yılın \n23 Eylül – 22 Ekim tarihleri arasında gerçekleşir. \nYıldız haritanızda Terazi burcu gezegen yerleşimine sahip olmasanız da yıl\n boyunca çeşitli gezegenler Terazi burcunu ziyaret ederek bu burcun \nhava enerjisini uyandırdığı için bu enerjiyi anlamak önemlidir.";
+
+                image.Image = burcresim.Images["terazi.png"];
+            }
+            else if (burc.Text == "Akrep")
+            {
+                comment.Text = "Zodyak’ın 12 burcundan sekizincisi olan Akrep burcunun döngüsü her yılın \n24 ekim – 23 kasım tarihleri arasında gerçekleşir. \nYıldız haritanızda Akrep burcu gezegen yerleşimine sahip olmasanız da yıl \nboyunca çeşitli gezegenler Akrep burcunu ziyaret ederek bu burcun \nsu enerjisini uyandırdığı için bu enerjiyi anlamak önemlidir.";
+
+                image.Image = burcresim.Images["akrep.png"];
+            }
+            else if (burc.Text == "Yay")
+            {
+                comment.Text = "Zodyak’ın 12 burcundan dokuzuncusu olan Yay burcunun döngüsü her yılın \n22 Kasım – 21 Aralık tarihleri arasında gerçekleşir. \nYıldız haritanızda Yay burcu gezegen yerleşimine sahip olmasanız da yıl \nboyunca çeşitli gezegenler Yay burcunu ziyaret ederek bu burcun \nateş enerjisini uyandırdığı için bu enerjiyi anlamak önemlidir.";
+
+                image.Image = burcresim.Images["yay.png"];
+            }
+            else if (burc.Text == "Oğlak")
+            {
+                comment.Text = "Zodyak’ın 12 burcundan onuncusu olan Oğlak burcunun döngüsü her yılın \n22 Aralık – 19 Ocak tarihleri arasında gerçekleşir. \nYıldız haritanızda Oğlak burcu gezegen yerleşimine sahip olmasanız da yıl \nboyunca çeşitli gezegenler Oğlak burcunu ziyaret ederek bu burcun \ntoprak enerjisini uyandırdığı için bu enerjiyi anlamak önemlidir.";
+
+                image.Image = burcresim.Images["oglak.png"];
+            }
+            else if (burc.Text == "Kova")
+            {
+                comment.Text = "Zodyak’ın 12 burcundan on birincisi olan Kova burcunun döngüsü her yılın \n21 Ocak – 18 Şubat tarihleri arasında gerçekleşir. \nYıldız haritanızda Kova burcu gezegen yerleşimine sahip olmasanız da yıl \nboyunca çeşitli gezegenler Kova burcunu ziyaret ederek bu burcun \nhava enerjisini uyandırdığı için bu enerjiyi anlamak önemlidir.";
+
+                image.Image = burcresim.Images["kova.png"];
+            }
+            else if (burc.Text == "Balık")
+            {
+                comment.Text = "Balık Zodyak’ın 12 burcundan on ikincisi olan Balık burcunun döngüsü her yılın \n19 Şubat – 20 Mart tarihleri arasında gerçekleşir. \nYıldız haritanızda Balık burcu gezegen yerleşimine sahip olmasanız da \nyıl boyunca çeşitli gezegenler Balık burcunu ziyaret ederek bu burcun \nsu enerjisini uyandırdığı için bu enerjiyi anlamak önemlidir.";
+
+                image.Image = burcresim.Images["balik.png"];
+            }
+        }
+
+        private void comment_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
